c150fe556e2d657311b969a8f091807bf19bd695
Varahiil <Uriilpro33@gmail.com>
2023-04-02 17:08:40 +0300

    Решение 6-ой задачи.

--- src/by/itstep/javatraining/revision/task/Task06.java
+++ src/by/itstep/javatraining/revision/task/Task06.java
@@ -23,6 +23,10 @@ package by.itstep.javatraining.revision.task;
 
 public class Task06 {
     public static boolean task06(int a, int b, int c) {
-        return false;
+        boolean answer = false;
+        if (a > 0 && b > 0 && c > 0 && a + b > c && b + c > a && c + a > b) {
+            answer = true;
+        }
+        return answer;
     }
 }
